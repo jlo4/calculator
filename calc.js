a88c9b03626aac10cc12c88462c82b819f16fc15
John Love <john.love4@gmail.com>
2017-06-29 22:36:03 +0300

    update css

--- calc.js
+++ calc.js
@@ -1,7 +1,11 @@
+const Saved = "0";
+const ScreenValue = "0";
+const MathValue = 0;
+const MaxDigits = 4;
+
 $(document).ready(function(){
 
 	$("span").on("click", function(){
-		var saved;
 		var num = $(this).text();
 		$("#screen").text(num);
 		var math = Number(num);
