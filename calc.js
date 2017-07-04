82381af64877804152ce1943e4d197ca6b77b80e
John Love <john.love4@gmail.com>
2017-07-04 17:37:22 +0300

    add math functions

--- calc.js
+++ calc.js
@@ -3,6 +3,22 @@ const ScreenValue = "0";
 const MathValue = 0;
 const MaxDigits = 4;
 
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
 $(document).ready(function(){
 
 	$("span").on("click", function(){
